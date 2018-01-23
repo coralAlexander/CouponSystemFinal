31b4751cf319eef749bd654c003ed8806ecf82f2
Alexander Coral <acoral@interwise.com>
2018-01-23 18:32:37 +0200

    Start working on POST request  v.6

--- coupon/src/main/java/httpClient/HttpClient.java
+++ coupon/src/main/java/httpClient/HttpClient.java
@@ -37,7 +37,9 @@ public class HttpClient {
 		//add request header
 		con.setRequestProperty("User-Agent", USER_AGENT);
 
+		
 		int responseCode = con.getResponseCode();
+		
 		//InputStream json = con.getInputStream();
 		System.out.println("\nSending 'GET' request to URL : " + url);
 		System.out.println("Response Code : " + responseCode);
@@ -53,6 +55,45 @@ public class HttpClient {
 		//print result
 		System.out.println(response.toString());
 		return response.toString();
+
+	}
+	
+	private void sendPost() throws Exception {
+
+		String url = "https://selfsolve.apple.com/wcResults.do";
+		URL obj = new URL(url);
+		HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+		//add reuqest header
+		con.setRequestMethod("POST");
+		con.setRequestProperty("User-Agent", USER_AGENT);
+		con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+		String urlParameters = "sn=C02G8416DRJM&cn=&locale=&caller=&num=12345";
+
+		// Send post request
+		con.setDoOutput(true);
+		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+		wr.writeBytes(urlParameters);
+		wr.flush();
+		wr.close();
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'POST' request to URL : " + url);
+		System.out.println("Post parameters : " + urlParameters);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		//print result
+		System.out.println(response.toString());
 
 	}
 }
